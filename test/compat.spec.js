088aa72cb18cc60b4f4564ba5d941ad38806138e
Gustav Nikolaj Olsen <gno@one.com>
2017-08-25 16:31:37 +0200

    Format with prettier and fix lint errors

--- test/compat.spec.js
+++ test/compat.spec.js
@@ -1,21 +1,19 @@
-const Koe = require('../')
-const expect = require('unexpected')
-  .clone()
-  .use(require('unexpected-koa'));
+const Koe = require('../');
+const expect = require('unexpected').clone().use(require('unexpected-koa'));
 
 it('should work as koa', () => {
-  const app = new Koe();
+    const app = new Koe();
 
-  app.use(async (ctx, next) => {
-    ctx.status = 418;
-    ctx.body = 'Foobar';
-  })
+    app.use(async (ctx, next) => {
+        ctx.status = 418;
+        ctx.body = 'Foobar';
+    });
 
-  return expect(app, 'to yield exchange', {
-    request: 'GET /',
-    response: {
-      statusCode: 418,
-      body: 'Foobar'
-    }
-  })
-})
+    return expect(app, 'to yield exchange', {
+        request: 'GET /',
+        response: {
+            statusCode: 418,
+            body: 'Foobar'
+        }
+    });
+});
--- test/useApp.spec.js
+++ test/useApp.spec.js
@@ -1,71 +1,69 @@
-const Koe = require('../')
-const expect = require('unexpected')
-  .clone()
-  .use(require('unexpected-koa'));
+const Koe = require('../');
+const expect = require('unexpected').clone().use(require('unexpected-koa'));
 
 it('should allow mounting an app as a middleware', () => {
-  const app = new Koe();
-  const wrapApp = new Koe();
+    const app = new Koe();
+    const wrapApp = new Koe();
 
-  app.use(async (ctx, next) => {
-    ctx.status = 418;
-    ctx.body = 'Foobar';
-  })
+    app.use(async (ctx, next) => {
+        ctx.status = 418;
+        ctx.body = 'Foobar';
+    });
 
-  wrapApp.use(app);
+    wrapApp.use(app);
 
-  return expect(wrapApp, 'to yield exchange', {
-    request: 'GET /',
-    response: {
-      statusCode: 418,
-      body: 'Foobar'
-    }
-  })
-})
+    return expect(wrapApp, 'to yield exchange', {
+        request: 'GET /',
+        response: {
+            statusCode: 418,
+            body: 'Foobar'
+        }
+    });
+});
 
 it('should mount an app with a path prefix', async () => {
-  const app = new Koe();
-  const wrapApp = new Koe();
+    const app = new Koe();
+    const wrapApp = new Koe();
 
-  app.use(async (ctx, next) => {
-    ctx.status = 418;
-    ctx.body = 'Foobar';
-  })
+    app.use(async (ctx, next) => {
+        ctx.status = 418;
+        ctx.body = 'Foobar';
+    });
 
-  wrapApp.use('/foo', app);
+    wrapApp.use('/foo', app);
 
-  await expect(wrapApp, 'to yield exchange', {
-    request: 'GET /foo',
-    response: {
-      statusCode: 418,
-      body: 'Foobar'
-    }
-  })
+    await expect(wrapApp, 'to yield exchange', {
+        request: 'GET /foo',
+        response: {
+            statusCode: 418,
+            body: 'Foobar'
+        }
+    });
 
-  await expect(wrapApp, 'to yield exchange', {
-    request: 'GET /',
-    response: 404
-  })
-})
+    await expect(wrapApp, 'to yield exchange', {
+        request: 'GET /',
+        response: 404
+    });
+});
 
 it('should mount a middleware with a path prefix', async () => {
-  const app = new Koe();
+    const app = new Koe();
 
-  app.use('/foo', async (ctx, next) => {
-    ctx.status = 418;
-    ctx.body = 'Foobar';
-  });
+    app.use('/foo', async (ctx, next) => {
+        ctx.status = 418;
+        ctx.body = 'Foobar';
+    });
 
-  await expect(app, 'to yield exchange', {
-    request: 'GET /foo',
-    response: {
-      statusCode: 418,
-      body: 'Foobar'
-    }
-  })
+    await expect(app, 'to yield exchange', {
+        request: 'GET /foo',
+        response: {
+            statusCode: 418,
+            body: 'Foobar'
+        }
+    });
 
-  await expect(app, 'to yield exchange', {
-    request: 'GET /',
-    response: 404
-  })
-})
+    await expect(app, 'to yield exchange', {
+        request: 'GET /',
+        response: 404
+    });
+});
