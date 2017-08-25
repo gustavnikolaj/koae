eab3aa67ebc4730b1890d0c6029321184ccee4d1
Gustav Nikolaj Olsen <gno@one.com>
2017-08-25 13:39:37 +0200

    Use app or middleware with path prefix

--- test/useApp.spec.js
+++ test/useApp.spec.js
@@ -22,3 +22,50 @@ it('should allow mounting an app as a middleware', () => {
     }
   })
 })
+
+it('should mount an app with a path prefix', async () => {
+  const app = new Koe();
+  const wrapApp = new Koe();
+
+  app.use(async (ctx, next) => {
+    ctx.status = 418;
+    ctx.body = 'Foobar';
+  })
+
+  wrapApp.use('/foo', app);
+
+  await expect(wrapApp, 'to yield exchange', {
+    request: 'GET /foo',
+    response: {
+      statusCode: 418,
+      body: 'Foobar'
+    }
+  })
+
+  await expect(wrapApp, 'to yield exchange', {
+    request: 'GET /',
+    response: 404
+  })
+})
+
+it('should mount a middleware with a path prefix', async () => {
+  const app = new Koe();
+
+  app.use('/foo', async (ctx, next) => {
+    ctx.status = 418;
+    ctx.body = 'Foobar';
+  });
+
+  await expect(app, 'to yield exchange', {
+    request: 'GET /foo',
+    response: {
+      statusCode: 418,
+      body: 'Foobar'
+    }
+  })
+
+  await expect(app, 'to yield exchange', {
+    request: 'GET /',
+    response: 404
+  })
+})
