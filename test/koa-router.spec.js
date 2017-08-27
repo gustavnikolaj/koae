ea698acf58e87ecbf0e9aa0053d37633fe6fecf2
Gustav Nikolaj Olsen <gno@one.com>
2017-08-27 18:40:20 +0200

    Add tests for interop with koa-router

---
+++ test/koa-router.spec.js
@@ -0,0 +1,32 @@
+const expect = require('unexpected').clone().use(require('unexpected-koa'));
+const Router = require('koa-router');
+const Koae = require('../');
+
+it('should be able to use a koa router instance', async () => {
+    const app = new Koae();
+    const router = new Router({ prefix: '/foo' });
+
+    router
+        .get('/bar', ctx => ctx.body = 'router>foo>bar')
+        .get('/baz', ctx => ctx.body = 'router>foo>baz');
+
+    app
+        .get('/foo/qux', ctx => ctx.body = 'router>foo>qux');
+
+    app.use(router.routes());
+
+    await expect(app, 'to yield exchange', {
+        request: 'GET /foo/bar',
+        response: 'router>foo>bar'
+    });
+
+    await expect(app, 'to yield exchange', {
+        request: 'GET /foo/baz',
+        response: 'router>foo>baz'
+    });
+
+    await expect(app, 'to yield exchange', {
+        request: 'GET /foo/qux',
+        response: 'router>foo>qux'
+    });
+});
